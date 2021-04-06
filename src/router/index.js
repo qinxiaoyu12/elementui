a25489a278eb292f928b27c2ba50f538d1bdae9b
24732 <2473203182@qq.com>
2021-04-06 23:39:00 +0800

    面包屑11

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from "@/components/Login";
 import home from "@/components/home";
-<<<<<<< HEAD
-import Welcome from "@/components/Welcome";
 import User from "@/components/user/User";
-=======
 import Welcome from "../components/Welcome";
->>>>>>> 013d0ba184e61a62926cc5ce489166a228a5cc99
 
 Vue.use(VueRouter)
 
@@ -17,20 +13,10 @@ const routes = [
   { path: '/home',
     component: home,
     redirect: '/welcome',
-<<<<<<< HEAD
     children: [{ path:'/welcome', component:Welcome },
-               { path: '/users', component: User}
+               { path: '/users', component: User},
     ]
   },
-
-=======
-    children: [{
-      path:'/welcome',
-      component: Welcome
-    }
-  ]
-  }
->>>>>>> 013d0ba184e61a62926cc5ce489166a228a5cc99
 ]
 
 const router = new VueRouter({
