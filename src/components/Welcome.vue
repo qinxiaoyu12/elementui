020cc3590e7a62500294a19af0d323eb1f910d50
tangyi <2473203182@qq.com>
2021-04-07 08:45:06 +0800

    20210407

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <div>
     <h3>Welcome</h3>
   </div>
@@ -9,23 +8,9 @@
 export default {
   name: "Welcome"
 }
-=======
-    <div class="welcome">
-        <h3>Welcome</h3>
-    </div>
-</template>
-
-<script>
-    export default {
-        name: "Welcome"
-    }
->>>>>>> 013d0ba184e61a62926cc5ce489166a228a5cc99
 </script>
 
 <style scoped>
 
-<<<<<<< HEAD
+
 </style>
-=======
-</style>
->>>>>>> 013d0ba184e61a62926cc5ce489166a228a5cc99
